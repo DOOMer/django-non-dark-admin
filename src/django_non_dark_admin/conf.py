0051c1209f9395ff5b1d5373398346642729cdfd
Andre Toscano <andre.toscano@gmail.com>
2021-12-27 11:43:41 -0800

    Change variable DISABLE_DARK_MODE to be defined by a function that checks the user preference.
    Update template tag to use the function to check if the dark mode should be disabled.
    
    Add documentation to have the dark mode disabled as a user preference.
    
    Clean up and black formatting.

--- src/django_non_dark_admin/conf.py
+++ src/django_non_dark_admin/conf.py
@@ -1,4 +1,14 @@
-
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
-DISABLE_DARK_MODE = int(getattr(settings, 'DISABLE_DARK_MODE', False))
+
+def disable_dark_mode(user=None):
+    user_model = get_user_model()
+    has_user_preference = hasattr(user_model, "disable_dark_mode")
+
+    DISABLE_DARK_MODE = (
+        getattr(user, "disable_dark_mode", False)
+        if has_user_preference and user
+        else getattr(settings, "DISABLE_DARK_MODE", False)
+    )
+    return int(DISABLE_DARK_MODE)
--- src/django_non_dark_admin/templatetags/admin_darkmode_switch.py
+++ src/django_non_dark_admin/templatetags/admin_darkmode_switch.py
@@ -1,13 +1,14 @@
-
 from django import template
-from ..conf import DISABLE_DARK_MODE
+from ..conf import disable_dark_mode
 
 register = template.Library()
 
 
-@register.inclusion_tag("django_non_dark_admin/darkmode_switch.html", takes_context=True)
+@register.inclusion_tag(
+    "django_non_dark_admin/darkmode_switch.html", takes_context=True
+)
 def admin_darkmode_switch(context):
-
-    context['disable_admin_dark'] = DISABLE_DARK_MODE
+    user = getattr(context["request"], "user", None)
+    context["disable_admin_dark"] = disable_dark_mode(user)
 
     return context
